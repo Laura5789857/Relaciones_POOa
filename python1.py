5fb013ad3b661abd5dff1da4ca8e82672969d865
lauranathalybeltran <lalislc2990871@gmail.com>
2026-09-22 11:02:42 -0500

    agrego empleado Juan Diego

---
+++ python1.py
@@ -0,0 +1,10 @@
+class empleado:
+    def_init__(self, nombre, cargo):
+    self.nombre = nombre 
+    self.cargo = cargo
+
+class tarea: 
+    def__init__(self, nombre, responsable):
+    self.nombre = nombre
+    self.responsable = responsable
+
