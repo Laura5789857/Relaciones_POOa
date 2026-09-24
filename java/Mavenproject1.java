feed456ce9085a9086505ddc15f9afa9e6779394
Laura5789857 <lalislc2990871@gmail.com>
2026-09-24 01:14:06 -0500

    Add files via upload

---
+++ java/Mavenproject1.java
@@ -0,0 +1,9 @@
+
+
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        Empleado empleado = new Empleado("Carlos","Ingeniero");
+        System.out.println(empleado.nombre + empleado.cargo);
+    }
+}
